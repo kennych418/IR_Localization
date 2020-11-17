ab79f0bdd1884da1445f11b3e98232cc73dff298
alexandergraening <agrae@DESKTOP-FHFL8FK.localdomain>
2020-11-16 17:45:03 -0800

    Some updates to the IMU code

--- IMU_DeadReckoning/IMU_DeadReckoning/IMU_DeadReckoning.ino
+++ IMU_DeadReckoning/IMU_DeadReckoning/IMU_DeadReckoning.ino
@@ -8,29 +8,61 @@ float gx_off, gy_off, gz_off;
 float mx, my, mz;
 float mx_off, my_off, mz_off;
 
+float x = 0;
+float y = 0;
+//float z = 0;
+//float phi = 0;
+float theta = 0;
+float v_x = 0;
+float v_y = 0;
+//float v_z = 0;
+
+float getM() {
+  return 0;
+}
+
+float getTheta() {
+  return 0;
+}
+
+bool updateCoordinates(float M, float theta) {
+  x = M*cos(theta);
+  y = M*sin(theta);
+  
+  return true;
+}
+
 bool calibrateOffestsIMU() {
   int start = millis();
-  int gx_max = 0;
-  int gy_max = 0;
-  int gz_max = 0;
-  int gx_min = 0;
-  int gy_min = 0;
-  int gz_min = 0;
-  int mx_max = 0;
-  int my_max = 0;
-  int mz_max = 0;
-  int mx_min = 0;
-  int my_min = 0;
-  int mz_min = 0;
-  while (millis() < start + 2000) {
+//  float gx_max = 0;
+//  float gy_max = 0;
+//  float gz_max = 0;
+//  float gx_min = 0;
+//  float gy_min = 0;
+//  float gz_min = 0;
+  int count = 0;
+  double gx_total = 0;
+  double gy_total = 0;
+  double gz_total = 0;
+  float mx_max = 0;
+  float my_max = 0;
+  float mz_max = 0;
+  float mx_min = 0;
+  float my_min = 0;
+  float mz_min = 0;
+  while (millis() < start + 1000) {
     IMU.readGyroscope(gx, gy, gz);
     IMU.readMagneticField(mx, my, mz);
-    if (gx > gx_max) gx_max = gx;
-    if (gx < gx_min) gx_min = gx;
-    if (gy > gy_max) gy_max = gy;
-    if (gy < gy_min) gy_min = gy;
-    if (gz > gz_max) gz_max = gz;
-    if (gz < gz_min) gz_min = gz;
+//    if (gx > gx_max) gx_max = gx;
+//    if (gx < gx_min) gx_min = gx;
+//    if (gy > gy_max) gy_max = gy;
+//    if (gy < gy_min) gy_min = gy;
+//    if (gz > gz_max) gz_max = gz;
+//    if (gz < gz_min) gz_min = gz;
+    gx_total += gx;
+    gy_total += gy;
+    gz_total += gz;
+    ++count;
     if (mx > mx_max) mx_max = mx;
     if (mx < mx_min) mx_min = mx;
     if (my > my_max) my_max = my;
@@ -38,9 +70,12 @@ bool calibrateOffestsIMU() {
     if (mz > mz_max) mz_max = mz;
     if (mz < mz_min) mz_min = mz;
   }
-  gx_off = (gx_max+gx_min)/2;
-  gy_off = (gy_max+gy_min)/2;
-  gz_off = (gz_max+gz_min)/2;
+//  gx_off = (gx_max+gx_min)/2;
+//  gy_off = (gy_max+gy_min)/2;
+//  gz_off = (gz_max+gz_min)/2;
+  gx_off = gx_total/count;
+  gy_off = gy_total/count;
+  gz_off = gz_total/count;
   mx_off = (mx_max+mx_min)/2;
   my_off = (my_max+my_min)/2;
   mz_off = (mz_max+mz_min)/2;
@@ -72,18 +107,24 @@ void loop() {
     Serial.print(az);
     IMU.readGyroscope(gx, gy, gz);
     Serial.print(", Gyr_x = ");
+//    Serial.print(gx);
     Serial.print(gx-gx_off);
     Serial.print(", Gyr_y = ");
+//    Serial.print(gy);
     Serial.print(gy-gy_off);
     Serial.print(", Gyr_z = ");
+//    Serial.print(gz);
     Serial.print(gz-gz_off);
     IMU.readMagneticField(mx, my, mz);
     Serial.print(", Mag_x = ");
-    Serial.print(mx-mx_off);
+    Serial.print(mx);
+//    Serial.print(mx-mx_off);
     Serial.print(", Mag_y = ");
-    Serial.print(my-my_off);
+    Serial.print(my);
+//    Serial.print(my-my_off);
     Serial.print(", Mag_z = ");
-    Serial.println(mz-mz_off);
+    Serial.println(mz);
+//    Serial.println(mz-mz_off);
     //int i = (int)x*100;
     //String st = String(i);
     //Serial.println(st);
