73e2e3809ccfdee55a4e971cdc7ac00115982b98
Kenny Chan <chankenny900@gmail.com>
2020-12-07 02:03:57 -0800

    Services added

--- Node/Node.ino
+++ Node/Node.ino
@@ -1,6 +1,7 @@
 #include <ArduinoBLE.h>
 
-#define BLE_UUID_TEST_SERVICE               "59920589-d782-492d-8beb-0d211d66312f"
+#define BLE_UUID_IDANDENABLE_SERVICE        "59920589-d782-492d-8beb-0d211d66312f"
+#define BLE_UUID_DATA_SERVICE               "19B10000-E8F2-537E-4F6C-D104768A1214"
 
 #define BLE_UUID_IDS                        "2813"
 #define BLE_UUID_ENABLE                     "2134"
@@ -12,9 +13,11 @@
 
 unsigned long ID = 0;
 
-BLEService testService( BLE_UUID_TEST_SERVICE );
+BLEService IDandEnableService( BLE_UUID_IDANDENABLE_SERVICE );
+BLEService DataService( BLE_UUID_DATA_SERVICE );
+
 BLEUnsignedLongCharacteristic IDsCharacteristic(BLE_UUID_IDS, BLEWrite|BLERead);
-BLEIntCharacteristic EnableCharacteristic(BLE_UUID_ENABLE, BLEWrite|BLERead);
+BLEUnsignedLongCharacteristic enableCharacteristic(BLE_UUID_ENABLE, BLEWrite|BLERead);
 BLEUnsignedLongCharacteristic timeCharacteristic( BLE_UUID_TIME, BLEWrite|BLERead);
 BLEUnsignedLongCharacteristic distCharacteristic( BLE_UUID_DIST, BLEWrite|BLERead);
 
@@ -34,8 +37,9 @@ void setup() {
 }
 
 void loop() {
+  BLE.setDeviceName("IRNode");
   BLE.setLocalName("IRNode");
-  BLE.scanForUuid( BLE_UUID_TEST_SERVICE );
+  BLE.scanForUuid( BLE_UUID_DATA_SERVICE );
   
   //Rand Delay and ID
   unsigned long start = millis();
@@ -92,15 +96,16 @@ void loop() {
     digitalWrite(LED_BUILTIN, HIGH);
     Serial.println("Entering Peripheral Mode");
 
-    BLE.setAdvertisedService( testService );
-    BLE.setAdvertisedServiceUuid( BLE_UUID_TEST_SERVICE );
-    testService.addCharacteristic( IDsCharacteristic );
-    testService.addCharacteristic( EnableCharacteristic );
-    testService.addCharacteristic( timeCharacteristic );
-    testService.addCharacteristic( distCharacteristic );
-    BLE.addService( testService );
+    BLE.setAdvertisedService( DataService );
+    BLE.setAdvertisedServiceUuid( BLE_UUID_DATA_SERVICE );
+    IDandEnableService.addCharacteristic( IDsCharacteristic );
+    IDandEnableService.addCharacteristic( enableCharacteristic );
+    DataService.addCharacteristic( timeCharacteristic );
+    DataService.addCharacteristic( distCharacteristic );
+    BLE.addService( IDandEnableService );
+    BLE.addService( DataService );
     IDsCharacteristic.writeValue( 1 );
-    EnableCharacteristic.writeValue( 2 );
+    enableCharacteristic.writeValue( 2 );
     timeCharacteristic.writeValue( 0 );
     distCharacteristic.writeValue( 0 );
     BLE.advertise();
@@ -109,10 +114,10 @@ void loop() {
       BLE.advertise();
       BLEDevice central = BLE.central();
       if(central){
-        if(central.localName() != "IRNode"){
-          Serial.println("Detected but Missed");
-          continue;
-        }
+        //if(central.deviceName() != "IRNode"){
+        //  Serial.println("Detected but Missed");
+        //  continue;
+        //}
         unsigned long count = 0;
         unsigned long ID = 0;
         while(central.connected()){
